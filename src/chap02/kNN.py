e888fe29eb9fe6cf7560c384e496866c9cb8eca5
unknown <67573710@qq.com>
2018-07-24 16:10:26 +0800

    chap2 end

--- src/chap02/kNN.py
+++ src/chap02/kNN.py
@@ -124,8 +124,54 @@ def classifyPerson():
     print(classifierResult)
     print("You will probably like this person: ", resultList[int(classifierResult)-1])
     
-    
-    
+'''
+    把图片转化为向量
+    这个方法仅对本章节附带的特殊格式存储的图片有效
+'''    
+def img2vector(filename):
+    returnVect = zeros((1, 1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0, 32 * i + j] = int(lineStr[j])
+    return returnVect
+
+'''
+    识别手写数字
+'''
+from os import listdir
+def handwritingClassTest():
+    hwLabels = []
+    trainingFileList = listdir('trainingDigits')
+    m = len(trainingFileList)
+    trainingMat = zeros((m, 1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i,:] = img2vector('trainingDigits/%s' % fileNameStr)
+    testFileList = listdir('testDigits')
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest, trainingMat,hwLabels,3)
+        print("The classifier came back with: %d, the real answer is: %d" %
+               (classifierResult, classNumStr))
+        if (classifierResult != classNumStr):
+            errorCount += 1.0
+    print("the total number of errors is: %d" % errorCount)
+    print("the total error rate is: %f" % (errorCount / float(mTest)))
+
+
+
+
+
     
     
     
---
+++ src/chap02/test2-6.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 24 16:05:58 2018
+
+@author: Administrator
+测试kNN算法识别手写数字
+"""
+
+import kNN
+kNN.handwritingClassTest()
